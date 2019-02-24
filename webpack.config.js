0aaf0f6287b7d3de2c4dfe02553e251706188e92
AntonOcean <tmath90@yandex.ru>
2019-02-24 23:39:33 +0300

    [T6] Добавлен webpack + eslinter

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const src = path.resolve(__dirname, 'public/src');
+const build = path.resolve(__dirname, 'dist');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(src, 'app.js'),
+    output: {
+        path: build,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
+};
