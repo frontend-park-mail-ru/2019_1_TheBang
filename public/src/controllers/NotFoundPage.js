a53f839947133eda2c7828ebcd387f586a82f4e1
Liza-Shch <scherbakova.liz@yandex.ru>
2019-03-10 23:45:58 +0300

    T19 Добавлена страница 404

--- public/src/controllers/NotFoundPage.js
+++ public/src/controllers/NotFoundPage.js
@@ -1,18 +1,30 @@
-import app from "../app";
+import NotFoundContent from "../components/NotFoundContent/NotFoundContent";
+import IndexController from "./section/IndexController";
 
-class NotFoundPage {
+
+class NotFoundPage extends IndexController{
     constructor() {
-        this.targetRender = app;
-    }
-
-    getTargetRender() {
-        return this.targetRender;
-    }
-
-    render()  {
-        return '<h1>Ничего не нашли</h1>' +
-            '<a href="#">home</a>'
+        super(NotFoundContent);
     }
 }
 
 export default NotFoundPage;
+
+// import app from "../app";
+
+// class NotFoundPage {
+//     constructor() {
+//         this.targetRender = app;
+//     }
+
+//     getTargetRender() {
+//         return this.targetRender;
+//     }
+
+//     render()  {
+//         return '<h1>Ничего не нашли</h1>' +
+//             '<a href="#">home</a>'
+//     }
+// }
+
+// export default NotFoundPage;
