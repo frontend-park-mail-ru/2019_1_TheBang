542f95160e03483d1301e884aed84f2f656c3cfd
Liza-Shch <scherbakova.liz@yandex.ru>
2019-03-15 13:04:36 +0300

    Т23 поправлены зависимости + константы видны глобально

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const src = path.resolve(__dirname, 'public/src');
+const css = path.resolve(__dirname, 'public/css');
 const build = path.resolve(__dirname, 'dist');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
@@ -22,13 +23,22 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
             },
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
+                    use: ['css-loader', 'sass-loader',
+                    {
+                        loader: 'sass-resources-loader',
+                        options: {
+                            resources: path.resolve(css, 'variable.scss'),
+                        }
+                    }]
                 })
             },
             {
