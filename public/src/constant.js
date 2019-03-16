b97fa6efbf41b68bf62d2501c0c986449f0ff876
AntonOcean <tmath90@yandex.ru>
2019-03-16 17:56:18 +0300

    [Т14] ссылка на бекенд

--- public/src/constant.js
+++ public/src/constant.js
@@ -1,7 +1,7 @@
 class Constant {
     constructor() {
         // this.backend = "http://127.0.0.1:8090/";
-        this.backend = "https://backend-for-anton.herokuapp.com/";
+        this.backend = "https://thee-bang-backend.herokuapp.com/";
 
         // Ограничение доступа к страницам
         this.unauthUser = "Anonymous";
