48fe6a1c310dfd8a56e69edc0de686570ecd4f03
AntonOcean <tmath90@yandex.ru>
2019-03-18 19:14:44 +0300

    [backend] поправил ссылку на бекенд

--- public/src/constant.js
+++ public/src/constant.js
@@ -1,7 +1,7 @@
 class Constant {
     constructor() {
         // this.backend = "http://127.0.0.1:8090/";
-        this.backend = "https://thee-bang-backend.herokuapp.com/";
+        this.backend = "https://stormy-fjord-97392.herokuapp.com/";
 
         // Ограничение доступа к страницам
         this.unauthUser = "Anonymous";
