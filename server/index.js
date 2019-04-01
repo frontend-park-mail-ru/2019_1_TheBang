039701fc49137b7f698dcb3089c7d0b317eb8ce5
AntonOcean <tmath90@yandex.ru>
2019-04-01 21:01:15 +0300

    [T38] set default heroku

--- server/index.js
+++ server/index.js
@@ -3,4 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.static('./'));
 
-app.listen();
+let port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
