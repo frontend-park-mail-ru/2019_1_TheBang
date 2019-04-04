67adc11e1d4d8991d956ab33bab63c209ca8690f
AntonOcean <tmath90@yandex.ru>
2019-04-04 18:47:19 +0300

    [T38] Единый импорт view

--- public/src/app.js
+++ public/src/app.js
@@ -1,14 +1,6 @@
 import 'css/style.scss';
 
-import LoginPage from 'src/views/LoginPage';
-import ProfilePage from 'src/views/ProfilePage';
-import HomePage from 'src/views/HomePage';
-import NotFoundPage from 'src/views/NotFoundPage';
-import UnAuthorizedPage from 'src/views/UnAuthorizedPage';
-import SignUpPage from 'src/views/SignUpPage';
-import AuthorsPage from 'src/views/AuthorsPage';
-import GamePage from 'src/views/GamePage';
-import LeadersPage from 'src/views/LeadersPage';
+import * as View from 'src/views/index';
 
 import Router from 'src/Router';
 import Store from 'src/Store';
@@ -19,47 +11,46 @@ import Network from 'src/network/Network';
 import PageEvents from 'src/events/PageEvents';
 import PermissionController from 'src/permission/PermissionController';
 import Permission from 'src/permission/Permission';
-import BasePage from 'src/views/BasePage';
 
 
 EventBus.on(PageEvents.UPDATE_STORE, Store.updateUser.bind(Store));
-EventBus.on(PageEvents.BASE_COMPONENTS_RENDER, BasePage.render);
+EventBus.on(PageEvents.BASE_COMPONENTS_RENDER, View.BasePage.render);
 
 EventBus.on(NetworkEvents.GET_USER, Network.getUser);
 EventBus.on(PageEvents.LOAD_PAGE, pageLoader);
 
 EventBus.on(NetworkEvents.SIGNUP, Network.signUpUser);
-EventBus.on(PageEvents.SIGNUP_SUCCESS, SignUpPage.Success);
-EventBus.on(PageEvents.SIGNUP_ERROR, SignUpPage.Error);
+EventBus.on(PageEvents.SIGNUP_SUCCESS, View.SignUpPage.Success);
+EventBus.on(PageEvents.SIGNUP_ERROR, View.SignUpPage.Error);
 
 EventBus.on(NetworkEvents.LOGIN, Network.loginUser);
-EventBus.on(PageEvents.LOGIN_SUCCESS, LoginPage.Success);
-EventBus.on(PageEvents.LOGIN_ERROR, LoginPage.Error);
+EventBus.on(PageEvents.LOGIN_SUCCESS, View.LoginPage.Success);
+EventBus.on(PageEvents.LOGIN_ERROR, View.LoginPage.Error);
 
 EventBus.on(NetworkEvents.UPDATE_PROFILE, Network.updateUser);
-EventBus.on(PageEvents.UPDATE_PROFILE_SUCCESS, ProfilePage.Success);
-EventBus.on(PageEvents.UPDATE_PROFILE_ERROR, ProfilePage.Error);
+EventBus.on(PageEvents.UPDATE_PROFILE_SUCCESS, View.ProfilePage.Success);
+EventBus.on(PageEvents.UPDATE_PROFILE_ERROR, View.ProfilePage.Error);
 
 EventBus.on(NetworkEvents.LOGOUT, Network.logoutUser);
-EventBus.on(PageEvents.LOGOUT_SUCCESS, HomePage.Success);
+EventBus.on(PageEvents.LOGOUT_SUCCESS, View.HomePage.Success);
 
 EventBus.on(NetworkEvents.GET_LEADERBOARD, Network.getLeaderboard);
-EventBus.on(PageEvents.GET_LEADERBOARD_SUCCESS, LeadersPage.Success);
-EventBus.on(PageEvents.GET_LEADERBOARD_ERROR, LeadersPage.Error);
+EventBus.on(PageEvents.GET_LEADERBOARD_SUCCESS, View.LeadersPage.Success);
+EventBus.on(PageEvents.GET_LEADERBOARD_ERROR, View.LeadersPage.Error);
 
 
 const router = new Router();
 
-router.addUrl('/', HomePage, 'index');
-router.addUrl('/profile', ProfilePage, 'profile', Permission.LOGIN_REQUIRED);
-router.addUrl('/auth', LoginPage, 'auth', Permission.ANONYMOUS);
-router.addUrl('/signup', SignUpPage, 'signup', Permission.ANONYMOUS);
-router.addUrl('/authors', AuthorsPage, 'authors');
-router.addUrl('/game', GamePage, 'game', Permission.LOGIN_REQUIRED);
-router.addUrl('/leaders', LeadersPage, 'leaders', Permission.LOGIN_REQUIRED);
+router.addUrl('/', View.HomePage, 'index');
+router.addUrl('/profile', View.ProfilePage, 'profile', Permission.LOGIN_REQUIRED);
+router.addUrl('/auth', View.LoginPage, 'auth', Permission.ANONYMOUS);
+router.addUrl('/signup', View.SignUpPage, 'signup', Permission.ANONYMOUS);
+router.addUrl('/authors', View.AuthorsPage, 'authors');
+router.addUrl('/game', View.GamePage, 'game', Permission.LOGIN_REQUIRED);
+router.addUrl('/leaders', View.LeadersPage, 'leaders', Permission.LOGIN_REQUIRED);
 
-router.addUrl('/not_found', NotFoundPage, 'not_found');
-router.addUrl('/unauthorized', UnAuthorizedPage, 'unauthorized');
+router.addUrl('/not_found', View.NotFoundPage, 'not_found');
+router.addUrl('/unauthorized', View.UnAuthorizedPage, 'unauthorized');
 
 /**
  * Подгрузка view для каждой страницы при переходе по url
