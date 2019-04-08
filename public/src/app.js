c85880035d69ad415c6e1c51ed251a0efb2246b3
AntonOcean <tmath90@yandex.ru>
2019-04-08 19:04:12 +0300

    [T41] fix service-worker

--- public/src/app.js
+++ public/src/app.js
@@ -88,9 +88,12 @@ function firstLoad() {
 window.addEventListener('hashchange', onPageLoad);
 window.addEventListener('DOMContentLoaded', firstLoad);
 
-
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js')
+		navigator.serviceWorker.register('/service-worker.js').then(registration => {
+			console.log('SW registered: ', registration);
+		}).catch(registrationError => {
+			console.log('SW registration failed: ', registrationError);
+		});
 	});
 }
