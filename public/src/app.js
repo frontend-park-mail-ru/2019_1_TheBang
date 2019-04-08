67aedbfce4320f83ddf302fbad630853b104a259
AntonOcean <tmath90@yandex.ru>
2019-04-08 19:08:44 +0300

    [T41] fix service-worker

--- public/src/app.js
+++ public/src/app.js
@@ -90,10 +90,6 @@ window.addEventListener('DOMContentLoaded', firstLoad);
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js').then(registration => {
-			console.log('SW registered: ', registration);
-		}).catch(registrationError => {
-			console.log('SW registration failed: ', registrationError);
-		});
+		navigator.serviceWorker.register('/service-worker.js')
 	});
 }
