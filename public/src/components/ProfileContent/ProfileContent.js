0a4cfee7b8c9dfc2063b170a251606fcc06eccdb
Liza-Shch <scherbakova.liz@yandex.ru>
2019-04-11 16:27:11 +0300

    Т45 некоторый апдейт

--- public/src/components/ProfileContent/ProfileContent.js
+++ public/src/components/ProfileContent/ProfileContent.js
@@ -1,11 +1,13 @@
-import 'src/components/ProfileContent/style.scss'
+import 'src/components/ProfileContent/style.scss';
+import 'src/pug-mixins/score/score.scss';
 import profileComponent from 'src/components/ProfileContent/template.pug';
 
 class ProfileContent {
 	constructor (data) {
 		this.nickname = data[0].nickname;
 		this.name = data[0].name;
-		this.score = data[0].score;
+		// this.score = data[0].score;
+		this.score = 123;
 		this.avatar = data[0].photo;
 	}
 
--- public/src/views/LeadersPage.js
+++ public/src/views/LeadersPage.js
@@ -28,30 +28,51 @@ class LeadersPage extends ContentMixin {
 	}
 
 	static onSuccess(data) {
-		const table = document.querySelector('table tbody');
+		const table = document.querySelector('.leaderboard');
 		table.innerHTML = '';
 
 		[].forEach.call(data, (item) => {
-			const tr = document.createElement('tr');
-			const tdPos = document.createElement('td');
-			const tdNickname = document.createElement('td');
-			const tdScore = document.createElement('td');
-			tdPos.innerText = item.posintion;
-			tr.appendChild(tdPos);
-			tdNickname.innerText = item.nickname;
-			tr.appendChild(tdNickname);
-			tdScore.innerText = item.score;
-			tr.appendChild(tdScore);
+			// const tr = document.createElement('tr');
+			// tr.classList.add('leaderboard', 'leaderboard__cell');
+			// const tdPos = document.createElement('td');
+			// const tdNickname = document.createElement('td');
+			// const tdScore = document.createElement('td');
+			// tdPos.innerText = item.posintion;
+			// tr.appendChild(tdPos);
+			// tdNickname.innerText = item.nickname;
+			// tr.appendChild(tdNickname);
+			// tdScore.innerText = item.score;
+			// tr.appendChild(tdScore);
 
-			table.appendChild(tr);
+			// table.appendChild(tr);
+			const row = document.createElement('div');
+			row.classList.add('leaderboard__row', 'leaderboard__cell');
+
+			const position = document.createElement('div');
+			position.innerText = item.position;
+
+			const nickname = document.createElement('div');
+			nickname.innerText = item.nickname;
+
+			const score = document.createElement('div');
+			score.innerText = item.score;
+
+			row.appendChild(position);
+			row.appendChild(nickname);
+			row.appendChild(score);
+
+			table.appendChild(row);
 		})
 	}
 
 	static onError() {
-		const table = document.querySelector('table tbody');
+		const table = document.querySelector('.leaderboard');
+		const paginator = document.querySelector('.paginator');
 		table.innerHTML = '';
-		const tr = document.createElement('tr');
-		tr.innerText = 'Не удалось загрузить данные, попробуйте позже'
+		paginator.innerHTML = '';
+		const error = document.createElement('div');
+		error.innerText = 'Не удалось загрузить данные, попробуйте позже';
+		table.appendChild(error);
 	}
 }
 
