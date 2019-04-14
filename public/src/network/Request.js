332c0e7a55a488cd68178aff609539181041d556
Liza-Shch <scherbakova.liz@yandex.ru>
2019-04-14 21:14:41 +0300

    Т45 Таблица лидеров

--- public/src/network/Request.js
+++ public/src/network/Request.js
@@ -1,5 +1,5 @@
-const backend = 'https://stormy-fjord-97392.herokuapp.com/';
-// const backend = 'http://127.0.0.1:8090/';
+// const backend = 'https://stormy-fjord-97392.herokuapp.com/';
+const backend = 'http://127.0.0.1:8090/';
 
 /**
  * Запросы на бекенд
