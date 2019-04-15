accf9f65d9e09da3c7d17d7ef85c70671a63cc04
Liza-Shch <scherbakova.liz@yandex.ru>
2019-04-15 11:56:12 +0300

    Т45 константа бэка

--- public/src/network/BackendResource.js
+++ public/src/network/BackendResource.js
@@ -1,5 +1,5 @@
 const BackendResource = {
-	BASE_HTTPS: 'http://127.0.0.1:8090/',// 'https://stormy-fjord-97392.herokuapp.com/',
+	BASE_HTTPS: /*'http://127.0.0.1:8090/',*/ 'https://stormy-fjord-97392.herokuapp.com/',
 	CHAT_WSS: 'wss://the-bang-chat.herokuapp.com/',
 	GAME_WSS: 'wss://the-bang-game-server.herokuapp.com/',
 	GAME_HTTPS: 'https://the-bang-game-server.herokuapp.com/'
