6b3ca6182ebfbf6bba183788a889e1e967ce42fd
Liza-Shch <scherbakova.liz@yandex.ru>
2019-04-15 12:13:02 +0300

    Т45 смена порта для тревиса

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('./dist'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
