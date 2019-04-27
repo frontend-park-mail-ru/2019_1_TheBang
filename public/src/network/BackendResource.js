f59ce39e36d17ff52b2d596117ebd6678b3b1253
a.baronskiy <a.baronskiy@mail.ru>
2019-04-27 10:56:57 +0300

    версия для деплоя на тачку

--- public/src/network/BackendResource.js
+++ public/src/network/BackendResource.js
@@ -1,9 +1,9 @@
 const BackendResource = {
-	BASE_HTTPS: 'https://stormy-fjord-97392.herokuapp.com/',
+	BASE_HTTPS: 'http://95.163.212.32:8001/',
 	// BASE_HTTPS: 'http://127.0.0.1:8090/',
 	CHAT_WSS: 'wss://the-bang-chat.herokuapp.com/',
-	GAME_WSS: 'wss://the-bang-game-server.herokuapp.com/',
-	GAME_HTTPS: 'https://the-bang-game-server.herokuapp.com/'
+	GAME_WSS: 'ws://95.163.212.32:8001',
+	GAME_HTTPS: 'http://95.163.212.32:8001/'
 };
 
 export default BackendResource;
