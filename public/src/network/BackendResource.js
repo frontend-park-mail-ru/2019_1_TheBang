0aba7bf40acc8785b00bcf0b27beced7651416e1
AntonOcean <tmath90@yandex.ru>
2019-04-30 20:10:07 +0300

    [T64] Фиксы

--- public/src/network/BackendResource.js
+++ public/src/network/BackendResource.js
@@ -3,19 +3,19 @@ const BackendResource = {
 	// GAME_WSS: 'wss://the-bang-game-server.herokuapp.com/',
 	// GAME_HTTPS: 'https://the-bang-game-server.herokuapp.com/',
 
-	// BASE_HTTPS: 'http://95.163.212.32:8001/',
-	// CHAT_WSS: 'ws://95.163.212.32:8003/',
-	// GAME_WSS: 'ws://95.163.212.32:8002/',
-	// GAME_HTTPS: 'http://95.163.212.32:8002/',
-	// CHAT_HTTPS: 'http://95.163.212.32:8003/',
+	BASE_HTTPS: 'http://95.163.212.32:8001/',
+	CHAT_WSS: 'ws://95.163.212.32:8003/',
+	GAME_WSS: 'ws://95.163.212.32:8002/',
+	GAME_HTTPS: 'http://95.163.212.32:8002/',
+	CHAT_HTTPS: 'http://95.163.212.32:8003/',
 
-	VK_CHAT: 'http://127.0.0.1:8001/',
-
-	BASE_HTTPS: 'http://127.0.0.1:8001/',
-	CHAT_WSS: 'ws://127.0.0.1:8003/',
-	GAME_WSS: 'ws://127.0.0.1:8002/',
-	GAME_HTTPS: 'http://127.0.0.1:8002/',
-	CHAT_HTTPS: 'http://127.0.0.1:8003/',
+	// VK_CHAT: 'http://127.0.0.1:8001/',
+	//
+	// BASE_HTTPS: 'http://127.0.0.1:8001/',
+	// CHAT_WSS: 'ws://127.0.0.1:8003/',
+	// GAME_WSS: 'ws://127.0.0.1:8002/',
+	// GAME_HTTPS: 'http://127.0.0.1:8002/',
+	// CHAT_HTTPS: 'http://127.0.0.1:8003/',
 };
 
 export default BackendResource;
