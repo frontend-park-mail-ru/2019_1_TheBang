b48cbe7e1943932889afd8cb749206b0725b4146
Liza-Shch <scherbakova.liz@yandex.ru>
2019-05-04 20:25:14 +0300

    T60 космос на экране игры

--- public/src/components/GameContent/GameContent.js
+++ public/src/components/GameContent/GameContent.js
@@ -2,6 +2,8 @@ import gameComponent from 'src/components/GameContent/template.pug';
 import 'src/components/GameContent/style.scss';
 import 'src/pug-mixins/animation/rotate.scss';
 import 'src/pug-mixins/controlls/controlls.scss';
+import 'src/pug-mixins/left-board/left-board.scss';
+import 'src/pug-mixins/right-board/right-board.scss';
 import Store from 'src/Store';
 
 class GameContent {
--- public/src/network/BackendResource.js
+++ public/src/network/BackendResource.js
@@ -3,17 +3,17 @@ const BackendResource = {
 	// GAME_WSS: 'wss://the-bang-game-server.herokuapp.com/',
 	// GAME_HTTPS: 'https://the-bang-game-server.herokuapp.com/',
 
-	BASE_HTTPS: 'http://95.163.212.32:8001/',
+	// BASE_HTTPS: 'http://95.163.212.32:8001/',
 	CHAT_WSS: 'ws://95.163.212.32:8003/',
-	GAME_WSS: 'ws://95.163.212.32:8002',
-	GAME_HTTPS: 'http://95.163.212.32:8002/',
+	// GAME_WSS: 'ws://95.163.212.32:8002',
+	// GAME_HTTPS: 'http://95.163.212.32:8002/',
 	CHAT_HTTPS: 'http://95.163.212.32:8003/',
 
-	VK_CHAT: 'http://127.0.0.1:8001/'
+	VK_CHAT: 'http://127.0.0.1:8001/',
 	// CHAT_HTTPS: 'test_xyu/',
-	// BASE_HTTPS: 'http://127.0.0.1:8001/',
-	// GAME_WSS: 'ws://127.0.0.1:8002/',
-	// GAME_HTTPS: 'http://127.0.0.1:8002/'
+	BASE_HTTPS: 'http://127.0.0.1:8001/',
+	GAME_WSS: 'ws://127.0.0.1:8002/',
+	GAME_HTTPS: 'http://127.0.0.1:8002/'
 };
 
 export default BackendResource;
