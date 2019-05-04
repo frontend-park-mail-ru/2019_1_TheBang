41785e90e0c73be1b2057df3bed02ec1f627ec5d
Liza-Shch <scherbakova.liz@yandex.ru>
2019-05-04 14:19:36 +0300

    T60 header

--- public/src/components/GameContent/GameContent.js
+++ public/src/components/GameContent/GameContent.js
@@ -2,10 +2,22 @@ import gameComponent from 'src/components/GameContent/template.pug';
 import 'src/components/GameContent/style.scss';
 import 'src/pug-mixins/animation/rotate.scss';
 import 'src/pug-mixins/controlls/controlls.scss';
+import Store from 'src/Store';
 
 class GameContent {
 	render() {
-		return gameComponent.call({}, {});
+		const user = Store.getUser();
+		let nickname = user.nickname;
+
+		if (!user.nickname) {
+			nickname = "undef";
+		}
+
+		const renderData = {
+			nickname: nickname
+		};
+
+		return gameComponent.call({}, renderData);
 	}
 }
 
