d18c78b24f8abec026cf88960047cebf5ab87f38
AntonOcean <tmath90@yandex.ru>
2019-05-05 14:30:56 +0300

    [cd] separate dev and prod deploy-config

---
+++ deploy-prod.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod 600 ./2019_1_TheBang_id_rsa.pem
+ssh-keyscan -H 95.163.212.32 >> ~/.ssh/known_hosts
+scp -i ./2019_1_TheBang_id_rsa.pem dist/* ubuntu@95.163.212.32:/var/www/the-bang-prod
