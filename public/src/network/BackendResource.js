68beb653df8fa361ee224dfcb10f8b0d8d721d8f
AntonOcean <tmath90@yandex.ru>
2019-05-28 18:38:56 +0300

    [nginx] СОздание прокси

--- public/src/network/BackendResource.js
+++ public/src/network/BackendResource.js
@@ -1,8 +1,8 @@
-const PORT_BASE = process.env.PORT_BASE || 8001;
+// const PORT_BASE = process.env.PORT_BASE || 8001;
 const PORT_GAME = process.env.PORT_GAME || 8002;
 const PORT_CHAT = process.env.PORT_CHAT || 8003;
 
-const HTTP = 'http://';
+// const HTTP = 'http://';
 const WS = 'ws://';
 const BACKEND_PREFIX = 'localhost:';
 const LOCAL_PREFIX = '127.0.0.1:';
@@ -13,12 +13,20 @@ if (process.env.NODE_ENV === 'local') {
 	CURRENT_MODE = LOCAL_PREFIX
 }
 
+// const BackendResource = {
+// 	BASE_HTTPS: HTTP + CURRENT_MODE + PORT_BASE + END_SLASH,
+// 	CHAT_WSS: WS + CURRENT_MODE + PORT_CHAT + END_SLASH,
+// 	GAME_WSS: WS + CURRENT_MODE + PORT_GAME + END_SLASH,
+// 	GAME_HTTPS: HTTP + CURRENT_MODE + PORT_GAME + END_SLASH,
+// 	CHAT_HTTPS: HTTP + CURRENT_MODE + PORT_CHAT + END_SLASH,
+// };
+
 const BackendResource = {
-	BASE_HTTPS: HTTP + CURRENT_MODE + PORT_BASE + END_SLASH,
+	BASE_HTTPS: '/api/auth/',
 	CHAT_WSS: WS + CURRENT_MODE + PORT_CHAT + END_SLASH,
 	GAME_WSS: WS + CURRENT_MODE + PORT_GAME + END_SLASH,
-	GAME_HTTPS: HTTP + CURRENT_MODE + PORT_GAME + END_SLASH,
-	CHAT_HTTPS: HTTP + CURRENT_MODE + PORT_CHAT + END_SLASH,
+	GAME_HTTPS:'/api/game/',
+	CHAT_HTTPS: '/api/chat/',
 };
 
 export default BackendResource;
